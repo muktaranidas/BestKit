11430af3d77cf04f5bc8aef55ade60c0ba17a133
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-04-27 13:42:58 -0700

    develope done

--- src/Pages/Home/BestkitDevelope/BestkitDevelope.js
+++ src/Pages/Home/BestkitDevelope/BestkitDevelope.js
@@ -1,23 +1,27 @@
 import React from "react";
-import bg from "../../../assets/BestkitDevelope/bg.png";
-import img from "../../../assets/BestkitDevelope/img.png";
+import bgImg from "../../../assets/BestkitDevelope/Bg.jpg";
 import Button from "../../Shared/Button/Button";
 
 const BestkitDevelope = () => {
   return (
-    <div className=" bg-bgColor my-8">
-      <div className=" widthClass hero-content  grid grid-cols-2 gap-2 mx-auto justify-between">
-        <div className="">
-          <h1 className="text-5xl font-bold">
-            Bestkit Develope Creative Solutions for Companies of All Niches
+    <div
+      className=""
+      style={{ background: `url(${bgImg})`, backgroundSize: "cover" }}
+    >
+      <div className="text-black"></div>
+      <div className="widthClass  mx-auto ">
+        <div className=" py-20">
+          <h1 className="mb-8 text-2xl font-bold">
+            Bestkit Develope Creative Solutions <br />
+            <span>for Companies of All Niches</span>
           </h1>
-          <p className="py-6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            varius enim in eros elementum tristique.
+          <p className="mb-8 w-[665px]">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
           </p>
-          <Button btnText={"Get Started Now"}></Button>
-        </div>{" "}
-        <img src={img} alt="" className="ml-72" />
+          <Button btnText="Get Started Now"></Button>
+        </div>
       </div>
     </div>
   );
