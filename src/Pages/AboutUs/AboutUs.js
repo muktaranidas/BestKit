3f60e593eafa28cabea5d02e20737f432b05b6c7
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-05-01 14:12:42 -0700

    done about

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AboutUs.css";
 import aboutImg from "../../../src/assets/about/different-coworkers-having-meeting 2.png";
 import bgImg from "../../../src/assets/about/Bg.png";
 import Button from "../Shared/Button/Button";
 
 const AboutUs = () => {
+  const [btn, setBtn] = useState("Our Mission");
+  console.log(btn);
+
+  function myFunction(text) {
+    setBtn(text);
+  }
+
   return (
-    <section className="">
-      <div className="  widthClass container flex flex-col  py-6 mx-auto sm:py-12 lg:py-24 lg:flex-row ">
-        <div className=" relative mr-[142px] flex  items-center justify-center py-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+    <section>
+      <div className="widthClass container flex flex-col  mx-auto  lg:flex-row ">
+        <div className=" relative mr-[142px] flex  justify-center  mt-8 lg:mt-0  ">
           <img
             src={aboutImg}
             alt=""
-            className="absolute left-[50px]  object-contain w-[250px] h-72 sm:h-80 lg:h-[565px] lg:w-[533px] "
+            className="absolute left-[50px]  top-[50px] object-contain w-[250px] h-72 sm:h-80 lg:h-[565px] lg:w-[533px] "
           />
           <img
             src={bgImg}
@@ -20,27 +27,34 @@ const AboutUs = () => {
             className="w-[525px] lg:h-[665px]  object-contain h-72 sm:h-80"
           />
         </div>
-        <div className="flex  flex-col text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+        <div className="flex py-[66px]  flex-col text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
           <p className="paragraph-style-tag">Learn About Us</p>
-          <h1 className="title">
-            <span className="">
-              Brainstorming Sessions Churning <br /> Out New Ideas
-            </span>
+          <h1 className="title w-[645px]">
+            Brainstorming Sessions Churning Out New Ideas
           </h1>
           <p className="paragraph-normal-tag">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
             varius enim in eros elementum tristique.
           </p>
 
-          {/* <div className="relative mt-[40px] mb-[30px]">
-            <button className="p-4  bg-[#EBF6FF] border-none w-1/2 font-bold  text-black text-right">
+          <div className="bg-[#EBF6FF] w-[284px]  p-[10px] mt-[40px] mb-[30px]">
+            <button
+              onClick={() => myFunction("Our Vision")}
+              className={`py-2 px-4  ${btn === "Our Vision" && "bg-blue"}  
+               border-none  font-bold  text-black text-right`}
+            >
               Our Vision
             </button>
 
-            <button className="p-2 m-2 absolute  left-1 bg-blue border-none w-32 font-bold  text-black text-right">
+            <button
+              onClick={() => myFunction("Our Mission")}
+              className={` py-2 px-4 ${btn === "Our Mission" && "bg-blue"} 
+               border-none font-bold  text-black text-right`}
+            >
               Our Mission
             </button>
-          </div> */}
+          </div>
+
           <p className="paragraph-normal-tag">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Imperdiet
             velit amet at suscipit accumsan orci. Luctus dolor vestibulum id
